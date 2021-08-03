284744be1d91be21a04b674bdf775524646f2f9c
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 13:18:58 -0400

    Initial Commit

---
+++ ActorExamples/ActorExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorExamplesApp.swift
+//  ActorExamples
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct ActorExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
