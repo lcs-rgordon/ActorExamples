d48a6cf42af296ec1b1f7d4d4627bf2fbe38e7b3
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 13:41:24 -0400

    Example of data race that's possible with a class... not so, when using an actor.

--- ActorExamples/ContentView.swift
+++ ActorExamples/ContentView.swift
@@ -7,22 +7,44 @@
 
 import SwiftUI
 
-// Actors have a queue of requests that are executed in the order they are received
-// Priorities for requests can be used to escalate requests
-// Only one piece of logic can access mutable state of an actor at a time (actor isolation)
-actor User {
+// Allows us to serialize a queue so that the data can be accessed safely
+actor URLCache {
     
-    let score = 10
+    // Private, so we hide the cache so it is accessed through the method
+    private var cache = [URL: Data]()
     
-    func printScore() {
-        print("My score is \(score)")
+    func data(for url: URL) async throws -> Data {
+        if let cached = cache[url] {
+            return cached
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        cache[url] = data
+        
+        return data
+    }
+}
+
+// Example of creating a data race – two tasks could be calling transfer at the same time on the same account
+// NOT GOOD! Transfer $100 from an account with only $100 in it... two transfers could pass the guard statement at the same time, allowing $200 to be transferred from the account that had only $100 in it... we end up with -$100, which should not be possible
+class BankAccount {
+    var balance: Decimal
+    
+    init(initialBalance: Decimal) {
+        balance = initialBalance
     }
     
-    func copyScore(from other: User) async {
-        // Steal another user's score
-        print(other.score)
+    func deposit(amount: Decimal) {
+        balance += amount
     }
     
+    func transfer(amount: Decimal, to other: BankAccount) {
+        guard balance > amount else {
+            return
+        }
+        balance -= amount
+        other.deposit(amount: amount)
+    }
 }
 
 struct ContentView: View {
